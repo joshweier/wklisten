1a6632de4f1fdc7b71cd5c4ef5cca57e9a96b5a4
Josh Weier <jweier@gmail.com>
2024-07-08 22:26:05 +1000

    fix: only store queried word if it was correctly answered
    fix: clean up bad cache names
    chore: clean up example sentences
    chore: don't store all known word data, only the ids
    fix: ignore blank input on return

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', async () => {
   const apiKey = 'c9dd4a7f-e0a3-4054-97ee-01640ecc27ac';
 
   // Temp clear out
-  
-  // localStorage.removeItem('wanikani_knownwords_cache');
+  // localStorage.removeItem('wklisten_knownwords_cache');
+  // localStorage.removeItem('wklisten_knownwords_cache_expiry');
 
   var consecutiveCorrect = 0;
   var firstPlay = true;
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   // Check for cached queries
   const cacheKey = 'wklisten_answered_cache';
   const cacheExpiryKey = 'wklisten_answered_cache_expiry';
-  const cacheExpiryTime = 3 * 24 * 60 * 60 * 1000;  // 3 days in milliseconds
+  const cacheExpiryTime = 30 * 24 * 60 * 60 * 1000;  // 3 days in milliseconds
 
   const cachedData = localStorage.getItem(cacheKey);
   const cacheExpiry = localStorage.getItem(cacheExpiryKey);
@@ -28,7 +28,6 @@ document.addEventListener('DOMContentLoaded', async () => {
   const showExampleButton = document.getElementById('show-example');
   const userInput = document.getElementById('user-input');
   const submitButton = document.getElementById('submit');
-  const showAnswerButton = document.getElementById('show-answer');
   const wordContainer = document.getElementById('word-container');
   const exampleSentence = document.getElementById('example-sentence');
   const exampleSentencesContainer = document.getElementById('example-sentences');
@@ -61,7 +60,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     submitButton.addEventListener(
         'click', () => checkAnswer(knownWords, apiKey));
-    showAnswerButton.addEventListener('click', showAnswer);
     nextWordButton.addEventListener('click', () => {
       userInput.value = '';
       userInput.classList.remove('input-correct', 'input-incorrect');
@@ -70,7 +68,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 
       // Hide everything
       showExampleButton.style.display = 'block';
-      showAnswerButton.style.display = 'none';
       nextWordButton.style.display = 'none';
       wordContainer.style.display = 'none';
       exampleSentence.style.display = 'none';
@@ -101,8 +98,8 @@ document.addEventListener('DOMContentLoaded', async () => {
   }
 
   async function updateQueriedCache() {
-    const cacheKey = 'wanikani_queried_cache';
-    const cacheExpiryKey = 'wanikani_queried_cache_expiry';
+    const cacheKey = 'wklisten_answered_cache';
+    const cacheExpiryKey = 'wklisten_answered_cache_expiry';
     const cacheExpiryTime = 3 * 24 * 60 * 60 * 1000;  // 3 days in milliseconds
 
     // Check for cache hit
@@ -119,76 +116,35 @@ document.addEventListener('DOMContentLoaded', async () => {
       const response = await fetch(
           nextUrl, {headers: {'Authorization': `Bearer ${apiKey}`}});
       const data = await response.json();
-      // console.log(data);
       knownWords.push(...data.data);
       nextUrl = data.pages.next_url;
     }
-    // console.log(knownWords);
     return knownWords;
   }
 
   // Get all known words we can display
   async function fetchKnownWords(apiKey) {
-    const cacheKey = 'wanikani_knownwords_cache';
-    const cacheExpiryKey = 'wanikani_knownwords_cache_expiry';
-    const cacheExpiryTime =
-        1 * 24 * 60 * 60 * 1000;  // 1 day in milliseconds
+    const cacheKey = 'wklisten_knownwords_cache';
+    const cacheExpiryKey = 'wklisten_knownwords_cache_expiry';
 
     // Check for cache hit
     const cachedData = localStorage.getItem(cacheKey);
     const cacheExpiry = localStorage.getItem(cacheExpiryKey);
     if (cachedData && cacheExpiry && new Date().getTime() < cacheExpiry) {
       console.log(`Known words cache hit.`);
-      const data = JSON.parse(cachedData)
-      return data.map(assignment => assignment.data.subject_id);
+      return JSON.parse(cachedData)
     }
 
     // Get all of our known words (dealing with pagination)
     const data = await fetchAllKnownWords(apiKey);
-    localStorage.setItem(cacheKey, JSON.stringify(data));
+    const words = data.map(assignment => assignment.data.subject_id);
+
+    localStorage.setItem(cacheKey, JSON.stringify(words));
     localStorage.setItem(
         cacheExpiryKey, new Date().getTime() + cacheExpiryTime);
 
-    return data.map(assignment => assignment.data.subject_id);
+    return words;
   }
-
-  // // Get all of our cached word data
-  // async function fetchCachedVocabWords(apiKey, levels) {
-  //   const cacheKey = 'wanikani_vocab_cache';
-  //   const cacheExpiryKey = 'wanikani_vocab_cache_expiry';
-  //   const cacheExpiryTime =
-  //       30 * 24 * 60 * 60 * 1000;  // 30 days in milliseconds
-
-  //   const cachedData = localStorage.getItem(cacheKey);
-  //   const cacheExpiry = localStorage.getItem(cacheExpiryKey);
-
-  //   if (cachedData && cacheExpiry && new Date().getTime() < cacheExpiry) {
-  //     console.log(`Vocab words cache hit.`);
-  //     return JSON.parse(cachedData);
-  //   }
-
-  //   const vocabWords = await fetchAllVocabWords(apiKey, levels);
-  //   localStorage.setItem(cacheKey, JSON.stringify(vocabWords));
-  //   localStorage.setItem(
-  //       cacheExpiryKey, new Date().getTime() + cacheExpiryTime);
-
-  //   return vocabWords;
-  // }
-
-  // // Get all valid vocab words for our level
-  // async function fetchAllVocabWords(apiKey, levels) {
-  //   let nextUrl = `https://api.wanikani.com/v2/subjects?types=vocabulary`;
-  //   const vocabWords = [];
-
-  //   while (nextUrl) {
-  //     const response = await fetch(
-  //         nextUrl, {headers: {'Authorization': `Bearer ${apiKey}`}});
-  //     const data = await response.json();
-  //     vocabWords.push(...data.data);
-  //     nextUrl = data.pages.next_url;
-  //   }
-  //   return vocabWords;
-  // }
 
   // Get a single new word
   async function fetchWord(filteredWords, apiKey) {
@@ -228,6 +184,7 @@ document.addEventListener('DOMContentLoaded', async () => {
       fetchWords.dataset.meaning =
           wordData.data.meanings.map(meaning => meaning.meaning).join(', ');
       fetchWords.dataset.word = wordData.data.characters;
+      fetchWords.dataset.wordId = wordData.id;
 
       // Show a random example sentence
       const exampleSentences = wordData.data.context_sentences;
@@ -239,13 +196,8 @@ document.addEventListener('DOMContentLoaded', async () => {
       alert('No audio available for this word.');
     }
 
-    // Save that we've queried this word
-    queriedWords.push(randomWord);
-    updateQueriedCache();
-
     nextWordButton.style.display = 'none';
     exampleSentencesContainer.style.display = 'none';
-    showAnswerButton.style.display = 'none';
     wordContainer.style.display = 'none';
     submitButton.style.display = 'inline-block';
   }
@@ -264,6 +216,10 @@ document.addEventListener('DOMContentLoaded', async () => {
   function checkAnswer(filteredWords, apiKey) {
     const userInputValue =
         userInput.value.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+    if (userInputValue === '')
+      return;
+
     const correctAnswers = JSON.parse(fetchWords.dataset.correctAnswer);
     const word = fetchWords.dataset.word;
 
@@ -281,12 +237,16 @@ document.addEventListener('DOMContentLoaded', async () => {
         playAudio("correct.wav");
       }
 
+      // Save that we've queried this word
+      queriedWords.push(fetchWords.dataset.wordId);
+      updateQueriedCache();
+
       showExampleSentences(false);
     } else {
       playAudio("incorrect.mp3");
       userInput.classList.add('input-incorrect');
       consecutiveCorrect = 0;
-      showAnswer();
+      showExampleSentences(true);
     }
 
     submitButton.style.display = 'none';
@@ -300,21 +260,24 @@ document.addEventListener('DOMContentLoaded', async () => {
     const meaning = fetchWords.dataset.meaning;
 
     exampleSentencesContainer.innerHTML = showMeaning ?
-        `<div class="meaning">${meaning}</div><h3>Example Sentences:</h3>` :
-        '<h3>Example Sentences:</h3>';
-    // FIXME: Pick a random one that's not the current helper
-    exampleSentences.forEach(sentence => {
-      const sentenceDiv = document.createElement('div');
-      sentenceDiv.textContent = `${sentence.ja} - ${sentence.en}`;
-      exampleSentencesContainer.appendChild(sentenceDiv);
-    });
+        `<div class="meaning">${
+            meaning}</div>` :  
+        '';
+
+    const sentence =
+        exampleSentences[Math.floor(Math.random() * exampleSentences.length)];
+
+    const sentenceDiv = document.createElement('div');
+    sentenceDiv.textContent = `${sentence.ja}`;
+    sentenceDiv.classList.add('example-text');
+    exampleSentencesContainer.appendChild(sentenceDiv);
+
+    const translationDiv = document.createElement('div');
+    translationDiv.textContent = `${sentence.en}`;
+    exampleSentencesContainer.appendChild(translationDiv);
+
     exampleSentencesContainer.style.display = 'block';
     nextWordButton.style.display = 'block';
-  }
-
-  // Show the answer
-  function showAnswer() {
-    showExampleSentences(true);
   }
 
   // Handle the enter key
