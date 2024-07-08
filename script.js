eb69e2522003939ff459cccfa466800c6bf3722d
Josh Weier <jweier@gmail.com>
2024-07-08 15:29:21 +1000

    fix: incorrect pagination
    chore: simplify known word queries
    feat: add sounds
    chore: clean-up of UI

--- script.js
+++ script.js
@@ -2,14 +2,33 @@ document.addEventListener('DOMContentLoaded', async () => {
   // FIXME: This needs to move out of here!
   const apiKey = 'c9dd4a7f-e0a3-4054-97ee-01640ecc27ac';
 
+  // Temp clear out
+  
+  // localStorage.removeItem('wanikani_knownwords_cache');
+
+  var consecutiveCorrect = 0;
+  var firstPlay = true;
+  var queriedWords = [];
+
+  // Check for cached queries
+  const cacheKey = 'wklisten_answered_cache';
+  const cacheExpiryKey = 'wklisten_answered_cache_expiry';
+  const cacheExpiryTime = 3 * 24 * 60 * 60 * 1000;  // 3 days in milliseconds
+
+  const cachedData = localStorage.getItem(cacheKey);
+  const cacheExpiry = localStorage.getItem(cacheExpiryKey);
+  if (cachedData && cacheExpiry && new Date().getTime() < cacheExpiry) {
+    queriedWords = JSON.parse(cachedData)
+  }
+
   // Cache common elements
   const loadingScreen = document.getElementById('loading-screen');
   const mainContent = document.getElementById('main-content');
   const playAudioButton = document.getElementById('play-audio');
+  const showExampleButton = document.getElementById('show-example');
   const userInput = document.getElementById('user-input');
   const submitButton = document.getElementById('submit');
   const showAnswerButton = document.getElementById('show-answer');
-  const feedback = document.getElementById('feedback');
   const wordContainer = document.getElementById('word-container');
   const exampleSentence = document.getElementById('example-sentence');
   const exampleSentencesContainer = document.getElementById('example-sentences');
@@ -26,33 +45,45 @@ document.addEventListener('DOMContentLoaded', async () => {
   try {
     // Figure out what level we are and combine it all together
     const userLevel = await fetchUserLevel(apiKey);
-    const levels = Array.from({length: userLevel}, (_, i) => i + 1).join(',');
     console.log(`User level: ${userLevel}`);
 
     const knownWords = await fetchKnownWords(apiKey);
-    const allVocabWords = await fetchCachedVocabWords(apiKey, levels);
-    const filteredWords = allVocabWords.filter(word => knownWords.includes(word.id));
+    console.log(`Known: ${knownWords.length}`);
 
     // Hide the loading screen and show the main content
     loadingScreen.style.display = 'none';
     mainContent.style.display = 'block';
 
+    showExampleButton.addEventListener('click', () => {
+      exampleSentence.style.display = 'block';
+      showExampleButton.style.display = 'none';
+    });
+
     submitButton.addEventListener(
-        'click', () => checkAnswer(filteredWords, apiKey));
+        'click', () => checkAnswer(knownWords, apiKey));
     showAnswerButton.addEventListener('click', showAnswer);
     nextWordButton.addEventListener('click', () => {
-      fetchWord(filteredWords, apiKey);
       userInput.value = '';
-      feedback.style.display = 'none';
+      userInput.classList.remove('input-correct', 'input-incorrect');
+      userInput.disabled = false;
+      userInput.focus();
+
+      // Hide everything
+      showExampleButton.style.display = 'block';
       showAnswerButton.style.display = 'none';
+      nextWordButton.style.display = 'none';
       wordContainer.style.display = 'none';
       exampleSentence.style.display = 'none';
+      exampleSentencesContainer.style.display = 'none';
       submitButton.style.display = 'inline-block';
+      
+      // Do this last as it's blocking
+      fetchWord(knownWords, apiKey);
     });
 
     document.addEventListener('keydown', handleEnterKey);
 
-    fetchWord(filteredWords, apiKey);
+    fetchWord(knownWords, apiKey);
   } catch (error) {
     console.error(error);
     alert('Failed to load vocabulary words.');
@@ -69,6 +100,33 @@ document.addEventListener('DOMContentLoaded', async () => {
     return Math.min(data.data.subscription.max_level_granted, data.data.level);
   }
 
+  async function updateQueriedCache() {
+    const cacheKey = 'wanikani_queried_cache';
+    const cacheExpiryKey = 'wanikani_queried_cache_expiry';
+    const cacheExpiryTime = 3 * 24 * 60 * 60 * 1000;  // 3 days in milliseconds
+
+    // Check for cache hit
+    localStorage.setItem(cacheKey, JSON.stringify(queriedWords));
+    localStorage.setItem(
+        cacheExpiryKey, new Date().getTime() + cacheExpiryTime);
+  }
+
+  async function fetchAllKnownWords(apiKey) {
+    let nextUrl = `https://api.wanikani.com/v2/assignments?subject_types=vocabulary&started=true`;
+    const knownWords = [];
+
+    while (nextUrl) {
+      const response = await fetch(
+          nextUrl, {headers: {'Authorization': `Bearer ${apiKey}`}});
+      const data = await response.json();
+      // console.log(data);
+      knownWords.push(...data.data);
+      nextUrl = data.pages.next_url;
+    }
+    // console.log(knownWords);
+    return knownWords;
+  }
+
   // Get all known words we can display
   async function fetchKnownWords(apiKey) {
     const cacheKey = 'wanikani_knownwords_cache';
@@ -76,64 +134,61 @@ document.addEventListener('DOMContentLoaded', async () => {
     const cacheExpiryTime =
         1 * 24 * 60 * 60 * 1000;  // 1 day in milliseconds
 
+    // Check for cache hit
     const cachedData = localStorage.getItem(cacheKey);
     const cacheExpiry = localStorage.getItem(cacheExpiryKey);
-
     if (cachedData && cacheExpiry && new Date().getTime() < cacheExpiry) {
       console.log(`Known words cache hit.`);
       const data = JSON.parse(cachedData)
-      return data.data.map(assignment => assignment.data.subject_id);
+      return data.map(assignment => assignment.data.subject_id);
     }
 
-    const response = await fetch(
-        'https://api.wanikani.com/v2/assignments?srs_stages=1,2,3,4,5,6,7,8,9,10',
-        {headers: {'Authorization': `Bearer ${apiKey}`}});
-    const data = await response.json();
-
+    // Get all of our known words (dealing with pagination)
+    const data = await fetchAllKnownWords(apiKey);
     localStorage.setItem(cacheKey, JSON.stringify(data));
     localStorage.setItem(
         cacheExpiryKey, new Date().getTime() + cacheExpiryTime);
 
-    return data.data.map(assignment => assignment.data.subject_id);
+    return data.map(assignment => assignment.data.subject_id);
   }
 
-  // Get all of our cached word data
-  async function fetchCachedVocabWords(apiKey, levels) {
-    const cacheKey = 'wanikani_vocab_cache';
-    const cacheExpiryKey = 'wanikani_vocab_cache_expiry';
-    const cacheExpiryTime =
-        30 * 24 * 60 * 60 * 1000;  // 30 days in milliseconds
+  // // Get all of our cached word data
+  // async function fetchCachedVocabWords(apiKey, levels) {
+  //   const cacheKey = 'wanikani_vocab_cache';
+  //   const cacheExpiryKey = 'wanikani_vocab_cache_expiry';
+  //   const cacheExpiryTime =
+  //       30 * 24 * 60 * 60 * 1000;  // 30 days in milliseconds
 
-    const cachedData = localStorage.getItem(cacheKey);
-    const cacheExpiry = localStorage.getItem(cacheExpiryKey);
+  //   const cachedData = localStorage.getItem(cacheKey);
+  //   const cacheExpiry = localStorage.getItem(cacheExpiryKey);
 
-    if (cachedData && cacheExpiry && new Date().getTime() < cacheExpiry) {
-      console.log(`Vocab words cache hit.`);
-      return JSON.parse(cachedData);
-    }
+  //   if (cachedData && cacheExpiry && new Date().getTime() < cacheExpiry) {
+  //     console.log(`Vocab words cache hit.`);
+  //     return JSON.parse(cachedData);
+  //   }
 
-    const vocabWords = await fetchAllVocabWords(apiKey, levels);
-    localStorage.setItem(cacheKey, JSON.stringify(vocabWords));
-    localStorage.setItem(
-        cacheExpiryKey, new Date().getTime() + cacheExpiryTime);
+  //   const vocabWords = await fetchAllVocabWords(apiKey, levels);
+  //   localStorage.setItem(cacheKey, JSON.stringify(vocabWords));
+  //   localStorage.setItem(
+  //       cacheExpiryKey, new Date().getTime() + cacheExpiryTime);
 
-    return vocabWords;
-  }
+  //   return vocabWords;
+  // }
 
-  // Get all valid vocab words for our level
-  async function fetchAllVocabWords(apiKey, levels) {
-    let nextUrl = `https://api.wanikani.com/v2/subjects?types=vocabulary&levels=${levels}`;
-    const vocabWords = [];
+  // // Get all valid vocab words for our level
+  // async function fetchAllVocabWords(apiKey, levels) {
+  //   let nextUrl = `https://api.wanikani.com/v2/subjects?types=vocabulary`;
+  //   const vocabWords = [];
 
-    while (nextUrl) {
-      const response = await fetch(
-          nextUrl, {headers: {'Authorization': `Bearer ${apiKey}`}});
-      const data = await response.json();
-      vocabWords.push(...data.data);
-      nextUrl = data.pages.next_url;
-    }
-    return vocabWords;
-  }
+  //   while (nextUrl) {
+  //     const response = await fetch(
+  //         nextUrl, {headers: {'Authorization': `Bearer ${apiKey}`}});
+  //     const data = await response.json();
+  //     vocabWords.push(...data.data);
+  //     nextUrl = data.pages.next_url;
+  //   }
+  //   return vocabWords;
+  // }
 
   // Get a single new word
   async function fetchWord(filteredWords, apiKey) {
@@ -144,11 +199,19 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     const randomWord =
         filteredWords[Math.floor(Math.random() * filteredWords.length)];
+
+    // Try again if we've already queried this word
+    if (queriedWords.includes(randomWord)) {
+      fetchWord(filteredWords, apiKey);
+      return;
+    }
+
     const response = await fetch(
-        `https://api.wanikani.com/v2/subjects/${randomWord.id}`,
+        `https://api.wanikani.com/v2/subjects/${randomWord}`,
         {headers: {'Authorization': `Bearer ${apiKey}`}});
 
     const wordData = await response.json();
+    // console.log(wordData);
     const numLines = wordData.data.pronunciation_audios.length;
     const audioUrl = wordData.data.pronunciation_audios[Math.floor(Math.random() * numLines)]?.url;
 
@@ -171,14 +234,17 @@ document.addEventListener('DOMContentLoaded', async () => {
       const randomSentence =
           exampleSentences[Math.floor(Math.random() * exampleSentences.length)];
       exampleSentence.textContent = randomSentence.ja;
-      exampleSentence.style.display = 'block';
+      // exampleSentence.style.display = 'block';
     } else {
       alert('No audio available for this word.');
     }
 
+    // Save that we've queried this word
+    queriedWords.push(randomWord);
+    updateQueriedCache();
+
     nextWordButton.style.display = 'none';
     exampleSentencesContainer.style.display = 'none';
-    feedback.style.display = 'none';
     showAnswerButton.style.display = 'none';
     wordContainer.style.display = 'none';
     submitButton.style.display = 'inline-block';
@@ -186,9 +252,12 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   // Play the audio for the word
   function playAudio(audioUrl) {
+    if (firstPlay) {
+      firstPlay = false;
+      return;
+    }
     const audio = new Audio(audioUrl);
     audio.play();
-    userInput.focus();
   }
 
   // Check the anser
@@ -202,18 +271,27 @@ document.addEventListener('DOMContentLoaded', async () => {
     wordContainer.style.display = 'block';
 
     if (correctAnswers.includes(userInputValue)) {
-      feedback.textContent = 'Correct!';
-      feedback.style.color = 'green';
-      feedback.style.display = 'block';
+      userInput.classList.add('input-correct');
+      consecutiveCorrect++;
+      const numAnswersPerLevel = 5;
+      if (consecutiveCorrect > 0 && consecutiveCorrect % numAnswersPerLevel === 0) {
+        playAudio("level-up.wav");
+      }
+      else {
+        playAudio("correct.wav");
+      }
+
       showExampleSentences(false);
     } else {
-      feedback.textContent = 'Incorrect!';
-      feedback.style.color = 'red';
-      feedback.style.display = 'block';
+      playAudio("incorrect.mp3");
+      userInput.classList.add('input-incorrect');
+      consecutiveCorrect = 0;
       showAnswer();
     }
 
     submitButton.style.display = 'none';
+    userInput.blur();
+    userInput.disabled = true;
   }
 
   // Show the example sentences
@@ -224,6 +302,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     exampleSentencesContainer.innerHTML = showMeaning ?
         `<div class="meaning">${meaning}</div><h3>Example Sentences:</h3>` :
         '<h3>Example Sentences:</h3>';
+    // FIXME: Pick a random one that's not the current helper
     exampleSentences.forEach(sentence => {
       const sentenceDiv = document.createElement('div');
       sentenceDiv.textContent = `${sentence.ja} - ${sentence.en}`;
@@ -246,6 +325,9 @@ document.addEventListener('DOMContentLoaded', async () => {
       } else {
         submitButton.click();
       }
+    }
+    else {
+      userInput.focus();
     }
   }
 });
